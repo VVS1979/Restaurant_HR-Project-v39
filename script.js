4a2c9119dd2d6148033bb3e97539ee5eb3e5808b
VVS <VVS1979@GMAIL.COM>
2020-12-17 03:02:19 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let age = 45
+let name = "Vivek"
+
+console.log(name, age);
+
+let arr1=['vivek', 34, 'etisalat']; 
+console.table(arr1);
+
+console.warn("this is a warning")
+console.error("error warning")
